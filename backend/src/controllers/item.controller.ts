e37a6c474e8ad17ec40639cf9af18f1579daaca5
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-01 17:35:25 +0530

    controller added

---
+++ backend/src/controllers/item.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { Item } from "../models/Item.model";
+
+
+export const getItems = async (_: Request, res: Response) => {
+const items = await Item.find();
+res.json(items);
+};
