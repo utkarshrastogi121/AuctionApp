117330bdb548b3235b3d796a8556b725a54de802
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-02-01 17:33:12 +0530

    item model added

---
+++ backend/src/models/Item.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Document } from "mongoose";
+
+
+export interface IItem extends Document {
+title: string;
+startingPrice: number;
+currentBid: number;
+highestBidder: string | null;
+endsAt: Date;
+}
+
+
+const ItemSchema = new Schema<IItem>({
+title: { type: String, required: true },
+startingPrice: Number,
+currentBid: Number,
+highestBidder: String,
+endsAt: Date,
+});
+
+
+export const Item = model<IItem>("Item", ItemSchema);
